0fe8b7e55c11f8c5501087900fb8831adc7a4db3
hbmarkofficial-business <hbmark.official@gmail.com>
2026-02-19 10:47:53 +0100

    Update ContactSection.tsx

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -55,7 +55,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-semibold text-foreground mb-1">Adresse</h4>
                   <p className="text-muted-foreground">
-                    Färberstraße 54, 90402 Nürnberg
+                    Bahnhofstraße 30B, 33602 Bielefeld
                   </p>
                 </div>
               </div>
@@ -68,10 +68,10 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-semibold text-foreground mb-1">Telefon</h4>
                   <a
-                    href="tel:015118311067"
+                    href="tel:01631679126"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    015118311067
+                    01631679126
                   </a>
                 </div>
               </div>
@@ -84,9 +84,8 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-semibold text-foreground mb-1">Öffnungszeiten</h4>
                   <p className="text-muted-foreground">
-                    Mo. – Fr. 10:00–20:00 Uhr
-                    <br />
-                    Sa. 10:00–19:00 Uhr
+                    Mo. – Sa. 8:30–19:00 Uhr
+                    
                   </p>
                 </div>
               </div>
@@ -99,7 +98,7 @@ export const ContactSection = () => {
               </Button>
 
               <Button variant="goldOutline" size="xl" asChild>
-                <a href="tel:+4917629117689">Jetzt anrufen</a>
+                <a href="tel:01631679126">Jetzt anrufen</a>
               </Button>
             </div>
           </AnimateOnScroll>
@@ -109,7 +108,7 @@ export const ContactSection = () => {
             <div className="relative">
               <div className="aspect-square lg:aspect-[4/5] rounded-sm overflow-hidden border border-border">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5187.871688976504!2d11.069183276892435!3d49.44792955903806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57a835434d4d%3A0xfba938ebd99d6f67!2sTop%20Salon%20-%20N%C3%BCrnberg!5e0!3m2!1sde!2sde!4v1769505288553!5m2!1sde!2sde"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19638.630117068562!2d8.495641174316402!3d52.02821710000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ba3d7a3dcfa987%3A0x5b1567e87a453f95!2sThe%20Friend%E2%80%99s%20Barber%20Shop!5e0!3m2!1sde!2sde!4v1771494829041!5m2!1sde!2sde"
                   width="100%"
                   height="100%"
                   style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
